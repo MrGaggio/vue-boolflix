95286ba55975022956bb275a7a6a574fe9d6815e
Stefano Ruggiu <steruggiu@gmail.com>
2022-01-19 17:18:10 +0000

    installato axios e abbozzata chiamata da collegare

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,24 +5,24 @@
 </template>
 
 <script>
-// import axios from "axios"
+import axios from "axios"
 
 export default {
-    name: "Main",
+  name: "Main",
 
-    data (){
-        return {
-
-    }
-}
-
-
-}
-
-
-
+  data() {
+    return {
+        
+    };
+  },
+  methods: {
+      searchFilm () {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=f0278b0dff056a07e75af2bb599a91bc&query=' + 'inputText che sta su Header')
+        .then()
+        .catch()
+      }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
