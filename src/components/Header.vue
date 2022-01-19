c68a8c103b4cd0a2372aace0c63fb61f6a21de15
Stefano Ruggiu <steruggiu@gmail.com>
2022-01-19 16:50:48 +0000

    inserita barra ricerca

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="search">
+        <label for="search"></label>
+        <input type="text" placeholder="search">
+        <button>cerca film</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+header {
+  height: 100px;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  <dir>
+
+  </dir>
+</template>
+
+<script>
+// import axios from "axios"
+
+export default {
+    name: "Main",
+
+    data (){
+        return {
+
+    }
+}
+
+
+}
+
+
+
+</script>
+
+<style>
+
+</style>
